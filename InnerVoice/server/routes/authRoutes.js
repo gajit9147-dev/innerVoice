8e062cb329ac571d1cb477884e4d17304b9f5a59
Ajeet Gupat <gajit9147@gmail.com>
2026-07-18 20:26:37 +0530

    middleware

--- InnerVoice/server/routes/authRoutes.js
+++ InnerVoice/server/routes/authRoutes.js
@@ -1,9 +1,18 @@
 import express from "express";
+import authMiddleware from "../middleware/authMiddleware.js";
 import { signup, login } from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
+
+router.get("/profile", authMiddleware, (req, res) => {
+  res.json({
+    success: true,
+    message: "Welcome to your profile!",
+    user: req.user,
+  });
+});
 
 export default router;
